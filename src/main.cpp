f53673c10854016d2cdb8dd888a36407b4702c2a
Emanuele Oriani <ema@scv.local>
2021-02-02 17:48:19 +0000

    Initial commit with stub library usage (libarchive and libxml2)

---
+++ src/main.cpp
@@ -0,0 +1,202 @@
+/*
+    This file is part of skyrim-pm.
+
+    skyrim-pm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    skyrim-pm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with skyrim-pm.  If not, see <https://www.gnu.org/licenses/>.
+ * */
+
+#include <iostream>
+#include <archive.h>
+#include <archive_entry.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <regex>
+#include <sstream>
+
+namespace {
+	class ar {
+		const std::string	fname_;
+		struct archive		*a_;
+
+		// unforutnately there is no way
+		// to "rewind" libarchive streams
+		// hence one has to close and 
+		// reopen each time
+		void reset_archive(void) {
+			if (a_) archive_read_free(a_);
+			a_ = archive_read_new();
+			if(ARCHIVE_OK != archive_read_support_filter_all(a_)) {
+				archive_read_free(a_);
+				throw std::runtime_error("Can't initialize libarchive - archive_read_support_filter_all");
+			}
+			if(ARCHIVE_OK != archive_read_support_format_all(a_)) {
+				archive_read_free(a_);
+				throw std::runtime_error("Can't initialize libarchive - archive_read_support_format_all");
+			}
+			if(ARCHIVE_OK != archive_read_open_filename(a_, fname_.c_str(), 10240)) {
+				archive_read_free(a_);
+				std::runtime_error((std::string("Can't open/read archive file '") + fname_ + "'").c_str()); 
+			}
+		}
+public:
+		ar(const char* fname) : fname_(fname), a_(0) {
+			reset_archive();
+		}
+
+		std::vector<std::string> get_content(void) {
+			std::vector<std::string>	out;
+			struct archive_entry	*entry = 0;
+			while(archive_read_next_header(a_, &entry) == ARCHIVE_OK) {
+				out.push_back(archive_entry_pathname(entry));
+			}
+			reset_archive();
+			return out;
+		}
+
+		bool extract_file(const std::string& fname, std::ostream& data_out) {
+			bool			rv = false;
+			struct archive_entry	*entry = 0;
+			while(archive_read_next_header(a_, &entry) == ARCHIVE_OK) {
+				if(fname == archive_entry_pathname(entry)) {
+					rv = true;
+					const static size_t	buflen = 2048;
+					char			buf[buflen];
+					la_ssize_t		rd = 0;
+					while((rd = archive_read_data(a_, &buf[0], buflen)) >= 0) {
+						if(0 == rd)
+							break;
+						if(rd > 0) data_out.write(&buf[0], rd);
+					}
+					if(rd < 0)
+						throw std::runtime_error((std::string("Corrupt stream, can't extract '") + fname + "' from archive").c_str());
+					break;
+				}
+			}
+			// reset the archive handle
+			reset_archive();
+			return rv;
+		}
+
+		~ar() {
+			archive_read_free(a_);
+		}
+	};
+
+	const std::string& find_ModuleConfig(const std::vector<std::string>& v, const std::string& f_ModuleConfig = "ModuleConfig.xml") {
+		const std::regex self_regex(std::string("/") + f_ModuleConfig + "$" , std::regex_constants::ECMAScript | std::regex_constants::icase);
+		for(const auto& i : v) {
+			if(std::regex_search(i, self_regex))
+			       return i;	
+		}
+		throw std::runtime_error("Can't find ModuleConfig.xml");
+	}
+
+	class ModCfgParser {
+		const std::string	s_;
+		xmlDocPtr		doc_;
+		xmlNode			*n_moduleName_,
+					*n_installSteps_,
+					*n_requiredInstallFiles_;
+
+		void print_element_names(std::ostream& ostr, xmlNode * a_node, const int level = 0) {
+			xmlNode *cur_node = NULL;
+			
+			for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+				if (cur_node->type == XML_ELEMENT_NODE) {
+					for(int j = 0; j < level; ++j)
+						ostr << '\t';
+					ostr << cur_node->name << " ";
+					for(xmlAttrPtr attr = cur_node->properties; NULL != attr; attr = attr->next) {
+						ostr << "[" << attr->name << "]{" << xmlGetProp(cur_node, attr->name) << "} ";
+					}
+					ostr << std::endl;
+				}
+        			print_element_names(ostr, cur_node->children, level+1);
+    			}
+		}
+
+		void init(void) {
+			auto root_element = xmlDocGetRootElement(doc_);
+			for (auto cur_node = root_element->children; cur_node; cur_node = cur_node->next) {
+				if (cur_node->type == XML_ELEMENT_NODE) {
+					if(std::string("moduleName") == (const char*)cur_node->name) {
+						n_moduleName_ = cur_node;
+					} else if(std::string("installSteps") == (const char*)cur_node->name) {
+						n_installSteps_ = cur_node;
+					} else if(std::string("requiredInstallFiles") == (const char*)cur_node->name) {
+						n_requiredInstallFiles_ = cur_node;
+					}
+				}
+    			}
+			if(!n_moduleName_ || !n_installSteps_)
+				throw std::runtime_error("ModuleConfig is missing 'moduleName' and/or 'installSteps'");
+		}
+
+		void display_name(std::ostream& ostr) {
+			ostr << "Module: " << xmlNodeGetContent(n_moduleName_) << std::endl;
+		}
+public:
+		ModCfgParser(const std::string& s) : s_(s), doc_(0), n_moduleName_(0), n_installSteps_(0), n_requiredInstallFiles_(0) {
+			doc_ = xmlReadMemory(s_.c_str(), s_.length(), "noname.xml", NULL, 0);
+			if(!doc_)
+				throw std::runtime_error("Can't parse XML of ModuleConfig");
+		}
+
+		void print_tree(std::ostream& ostr) {
+			auto root_element = xmlDocGetRootElement(doc_);
+    			print_element_names(ostr, root_element);
+		}
+
+		void execute(std::ostream& ostr, std::istream& istr) {
+			init();
+			display_name(ostr);
+		}
+
+		~ModCfgParser() {
+			if(doc_)
+				xmlFreeDoc(doc_);
+		}
+	};
+}
+
+int main(int argc, char *argv[]) {
+	try {
+		if(argc < 1)
+			throw std::runtime_error("Need to provide a FOMOD compatible archive name");
+
+		ar			a(argv[1]);
+		// get list of files/directories
+		const auto		cnt = a.get_content();
+		// get and load the ModuleConfig.xml file
+		const auto&		modcfg = find_ModuleConfig(cnt);
+		std::stringstream	sstr;
+		if(!a.extract_file(modcfg, sstr))
+			throw std::runtime_error("Can't extract ModuleConfig from archive");
+		// parse the XML
+		ModCfgParser		mcp(sstr.str());
+		// execute it
+		mcp.execute(std::cout, std::cin);
+		//mcp.print_tree(std::cout);
+
+		// cleanup the xml2 library structures
+		xmlCleanupParser();
+
+	} catch(const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	} catch(...) {
+		std::cerr << "Unknown exception" << std::endl;
+	}
+}
